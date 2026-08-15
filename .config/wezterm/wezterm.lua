7cc1cce85bf74d24a380cc65653081b9bac980c4
Riku Yukimaru <setsugan.robocon@gmail.com>
2026-08-16 08:10:00 +0900

    weztermに設定の自動反映を追加

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 local wezterm = require 'wezterm'
+
 local config = wezterm.config_builder()
+config.automatically_reload_config = true
 
 config.font = wezterm.font 'Moralerspace Argon JPDOC'
 config.font_size = 12.0
